877adb525840b73af80410fc9c8f87c8d59c3319
umumkorowinner <umukorowinner990@gmail.com>
2022-07-05 22:38:30 -0700

    debbugged

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-void positive_or_negative(int i);
-
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - main
+ * Return: 0
+ */
 
 int main(void)
 {
-        int i;
+int i;
+i = 0;
 
-        i = 0;
-        positive_or_negative(i);
+positive_or_negative(i);
 
-        return (0);
+return (0);
+
 }
